25acfd78e22fc4f7160e336d334d4f3a96cc87af
nyxro09 <169052725+nyxro09@users.noreply.github.com>
2026-05-16 16:45:29 +0200

    Update header.php

--- Phase_3/header.php
+++ Phase_3/header.php
@@ -38,44 +38,44 @@ if (isset($_SESSION['user_id'])) {
         </a>
         <nav>
     <ul class="nav-links">
-        <li><a href=\"Accueil.php\">ACCUEIL</a></li>
-        <li><a href=\"Carte.php\">LA CARTE</a></li>
+        <li><a href="Accueil.php">ACCUEIL</a></li>
+        <li><a href="Carte.php">LA CARTE</a></li>
 
         <?php 
         // Si la personne N'EST PAS connectée
         if (!isset($_SESSION['role'])) : 
         ?>
-            <li><a href=\"Login.php\">CONNEXION</a></li>
-            <li><a href=\"Register.php\">INSCRIPTION</a></li>
+            <li><a href="Login.php">CONNEXION</a></li>
+            <li><a href="Register.php">INSCRIPTION</a></li>
 
         <?php 
         // Si c'est un CLIENT
         elseif ($_SESSION['role'] === 'client') : 
         ?>
-            <li><a href=\"Profil.php\">MON PROFIL</a></li>
-            <li><a href=\"Panier.php\">MON PANIER</a></li>
-            <li><a href=\"Logout.php\">DÉCONNEXION</a></li>
+            <li><a href="Profil.php">MON PROFIL</a></li>
+            <li><a href="Panier.php">MON PANIER</a></li>
+            <li><a href="Logout.php">DÉCONNEXION</a></li>
 
         <?php 
         // Si c'est le RESTAURATEUR
         elseif ($_SESSION['role'] === 'restaurateur') : 
         ?>
-            <li><a href=\"Restaurateur.php\">GESTION COMMANDES</a></li>
-            <li><a href=\"Logout.php\">DÉCONNEXION</a></li>
+            <li><a href="Restaurateur.php">GESTION COMMANDES</a></li>
+            <li><a href="Logout.php">DÉCONNEXION</a></li>
 
         <?php 
         // Si c'est un LIVREUR
         elseif ($_SESSION['role'] === 'livreur') : 
         ?>
-            <li><a href=\"Livreur.php\">MA TOURNÉE</a></li>
-            <li><a href=\"Logout.php\">DÉCONNEXION</a></li>
+            <li><a href="Livreur.php">MA TOURNÉE</a></li>
+            <li><a href="Logout.php">DÉCONNEXION</a></li>
 
         <?php 
         // Si c'est l'ADMINISTRATEUR
         elseif ($_SESSION['role'] === 'admin') : 
         ?>
-            <li><a href=\"Admin.php\">DASHBOARD ADMIN</a></li>
-            <li><a href=\"Logout.php\">DÉCONNEXION</a></li>
+            <li><a href="Admin.php">DASHBOARD ADMIN</a></li>
+            <li><a href="Logout.php">DÉCONNEXION</a></li>
         <?php endif; ?>
 
         <li>
