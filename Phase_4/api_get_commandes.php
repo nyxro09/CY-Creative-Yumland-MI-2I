0e2c6169b5adf91325c38318273a53d5ca82eb60
Craapul95 <167770049+Craapul95@users.noreply.github.com>
2026-05-31 15:57:06 +0200

    Update api_get_commandes.php

--- Phase_4/api_get_commandes.php
+++ Phase_4/api_get_commandes.php
@@ -1,5 +1,11 @@
-<?php
+<?php session_start();
 header('Content-Type: application/json; charset=utf-8');
-echo file_get_contents('data/commandes.json');
-exit();
-?>
+require_once('fonctions.php');
+$commandes=getCommandes();
+$role = $_SESSION['role'] ?? '';
+if ($role === 'livreur'){
+    $commandes = array_filter($commandes, function($cmd) {
+        return  $cmd['statut'] === 'prete' || ($cmd['statut'] === 'livraison' && ($cmd['livreur_id'] ?? '') === $_SESSION['user_id']);
+    });
+}
+echo json_encode(array_values($commandes));
